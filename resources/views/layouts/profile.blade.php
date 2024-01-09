bf141447acb7bf332bf5f11f5295df76c4c509b4
adamarnap <adamarnap0117@gmail.com>
2024-01-09 22:30:24 +0700

    added fontawesome

--- resources/views/layouts/profile.blade.php
+++ resources/views/layouts/profile.blade.php
@@ -18,6 +18,11 @@
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,600;1,700&family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Raleway:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
 
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/fontawesome.min.css" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css" />
+
+  {{-- <link type="text/css" rel="stylesheet" href="{{ mix('css/app.css') }}"> --}}
+
   <!-- Vendor CSS Files -->
   <link href="{{asset('vendor/impact/assets/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
   <link href="{{asset('vendor/impact/assets/vendor/bootstrap-icons/bootstrap-icons.css')}}" rel="stylesheet">
--- resources/views/profile/projects/detail.blade.php
+++ resources/views/profile/projects/detail.blade.php
@@ -56,7 +56,7 @@
               <div class="col-lg-3 col-md-6">
                 <div class="service-item  position-relative">
                   <div class="icon">
-                    <i class="bi bi-activity"></i>
+                    <i class="fa fa-bed"></i>
                   </div>
                   <h3>Nesciunt Mete</h3>
                   <p>Provident nihil minus qui consequatur non omnis maiores. Eos accusantium minus dolores iure perferendis tempore et consequatur.</p>
