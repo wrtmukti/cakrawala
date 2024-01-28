8b7dbeffa846e88b0ef01b9a09d46aa29bd1e64b
unknown <wrtmukti@gmail.com>
2024-01-28 15:48:57 +0700

    FAQ

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -363,14 +363,28 @@
                                 <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
                                     data-bs-target="#faq-content-1">
                                     <span class="num">1.</span>
-                                    Non consectetur a erat nam at lectus urna duis?
+                                    Apa Yang Harus Ditanyakan Ketika Akan Membeli Rumah?
                                 </button>
                             </h3>
                             <div id="faq-content-1" class="accordion-collapse collapse" data-bs-parent="#faqlist">
                                 <div class="accordion-body">
-                                    Feugiat pretium nibh ipsum consequat. Tempus iaculis urna id volutpat lacus laoreet
-                                    non curabitur gravida. Venenatis lectus magna fringilla urna porttitor rhoncus dolor
-                                    purus non.
+                                    <ul>
+                                        <li>
+                                            Fasat dan Denah Rumah
+                                        </li>
+                                        <li>
+                                            Lokasi Perumahan
+                                        </li>
+                                        <li>
+                                            Syarat Pengambilan Rumah
+                                        </li>
+                                        <li>
+                                            Metode Pengambilan Rumah
+                                        </li>
+                                        <li>
+                                            Proses Pengambilan Rumag Sampai Serah Terima Kunci
+                                        </li>
+                                    </ul>
                                 </div>
                             </div>
                         </div><!-- # Faq item-->
@@ -380,15 +394,19 @@
                                 <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
                                     data-bs-target="#faq-content-2">
                                     <span class="num">2.</span>
-                                    Feugiat scelerisque varius morbi enim nunc faucibus a pellentesque?
+                                    Dimana Saja Proyek Perumahan Cakrawala?
                                 </button>
                             </h3>
                             <div id="faq-content-2" class="accordion-collapse collapse" data-bs-parent="#faqlist">
                                 <div class="accordion-body">
-                                    Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi. Id interdum
-                                    velit laoreet id donec ultrices. Fringilla phasellus faucibus scelerisque eleifend
-                                    donec pretium. Est pellentesque elit ullamcorper dignissim. Mauris ultrices eros in
-                                    cursus turpis massa tincidunt dui.
+                                   <p><a href="https://cakrawalaproperty.com/project/detail/1"> 1. Cakrawala Utama Residence 1 – Mandan, Sukoharjo, Jawa Tengah</a></p>
+                                   <p><a href="https://cakrawalaproperty.com/project/detail/1"> 2. Cakrawala Utama Residence 2 – Mandan, Sukoharjo, Jawa Tengah</a></p>
+                                   <p><a href="https://cakrawalaproperty.com/project/detail/1"> 3. Cakrawala Utama Residence 3 – Jetis, Baki, Sukoharjo, Jawa Tengah</a></p>
+                                   <p><a href="https://cakrawalaproperty.com/project/detail/1"> 4. Cakrawala Utama Residence 4 – Karangturi, Gondangrejo, Karanganyar, Jawa Tengah</a></p>
+                                   <p><a href="https://cakrawalaproperty.com/project/detail/1"> 5. Cakrawala Utama Residence 5 – Jetis, Baki, Sukoharjo, Jawa Tengah</a></p>
+                                   <p><a href="https://cakrawalaproperty.com/project/detail/1"> 6. Cakrawala Utama Residence 6 – Kwangen, Gemolong, Sragen, Jawa Tengah</a></p>
+                                   <p><a href="https://cakrawalaproperty.com/project/detail/1"> 7. Cakrawala Utama Residence 7 – Jetis, Baki, Sukoharjo, Jawa Tengah</a></p>
+      
                                 </div>
                             </div>
                         </div><!-- # Faq item-->
@@ -398,15 +416,40 @@
                                 <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
                                     data-bs-target="#faq-content-3">
                                     <span class="num">3.</span>
-                                    Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi?
+                                    Apa Saja Syarat Pengajuan KPR?
                                 </button>
                             </h3>
                             <div id="faq-content-3" class="accordion-collapse collapse" data-bs-parent="#faqlist">
                                 <div class="accordion-body">
-                                    Eleifend mi in nulla posuere sollicitudin aliquam ultrices sagittis orci. Faucibus
-                                    pulvinar elementum integer enim. Sem nulla pharetra diam sit amet nisl suscipit.
-                                    Rutrum tellus pellentesque eu tincidunt. Lectus urna duis convallis convallis
-                                    tellus. Urna molestie at elementum eu facilisis sed odio morbi quis
+                                    <ul>
+                                        <li>
+                                            KTP
+                                        </li>
+                                        <li>
+                                            KK
+                                        </li>
+                                        <li>
+                                            Buku Nikah
+                                        </li>
+                                        <li>
+                                            NPWP
+                                        </li>
+                                        <li>
+                                            Surat Keterangan Kerja
+                                        </li>
+                                        <li>
+                                            Slip Gaji 3 Bulan Terakhir
+                                        </li>
+                                        <li>
+                                            Rekening Koran 3 Bulan Terakhir
+                                        </li>
+                                        <li>
+                                            Bpjs
+                                        </li>
+                                        <li>
+                                            Pas Photo 3x4
+                                        </li>
+                                      </ul>
                                 </div>
                             </div>
                         </div><!-- # Faq item-->
@@ -416,15 +459,12 @@
                                 <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
                                     data-bs-target="#faq-content-4">
                                     <span class="num">4.</span>
-                                    Ac odio tempor orci dapibus. Aliquam eleifend mi in nulla?
+                                    Bagaimana tentang angsuran pembelian rumah KPR di Cakrawala?
                                 </button>
                             </h3>
                             <div id="faq-content-4" class="accordion-collapse collapse" data-bs-parent="#faqlist">
                                 <div class="accordion-body">
-                                    Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi. Id interdum
-                                    velit laoreet id donec ultrices. Fringilla phasellus faucibus scelerisque eleifend
-                                    donec pretium. Est pellentesque elit ullamcorper dignissim. Mauris ultrices eros in
-                                    cursus turpis massa tincidunt dui.
+                                    Perumahan Subsidi Cakrawala memiliki bunga angsuran flat sampai lunas
                                 </div>
                             </div>
                         </div><!-- # Faq item-->
@@ -434,15 +474,17 @@
                                 <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
                                     data-bs-target="#faq-content-5">
                                     <span class="num">5.</span>
-                                    Tempus quam pellentesque nec nam aliquam sem et tortor consequat?
+                                    Kenapa harus memilih Cakrawala?
                                 </button>
                             </h3>
                             <div id="faq-content-5" class="accordion-collapse collapse" data-bs-parent="#faqlist">
                                 <div class="accordion-body">
-                                    Molestie a iaculis at erat pellentesque adipiscing commodo. Dignissim suspendisse in
-                                    est ante in. Nunc vel risus commodo viverra maecenas accumsan. Sit amet nisl
-                                    suscipit adipiscing bibendum est. Purus gravida quis blandit turpis cursus in
-                                </div>
+                                    <p>1. Legalitas Aman</p>
+                                    <p>2. Proses Cepat</p>
+                                    <p>3. Bahan Material Berkualitas</p>
+                                    <p>4. Lokasi Strategis</p>
+                                    <p>5. Dibantu Sampai Rumah Ditempati</p>
+                                </div>                                
                             </div>
                         </div><!-- # Faq item-->
 
